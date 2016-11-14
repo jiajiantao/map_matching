211c94e60c8b6158291c069af9bb8da3a8884739
jiajiantao <=>
2016-11-14 16:58:01 +0800

    hangzhou

--- src/main/MapMatching.java
+++ src/main/MapMatching.java
@@ -9,67 +9,76 @@ import output.Output;
 import tool.Tool;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
-public class MapMatching{
-	
-	public static void main(String args[]) throws IOException, InterruptedException{
+public class MapMatching {
+
+	public static void main(String args[]) throws IOException, InterruptedException {
 		Tool.log("Start...");
-		//**************** Generate Map *****************************
-		String mapFile = "./shenzhen2013-shifted-formated.osm";
+		// **************** Generate Map *****************************
+		//String mapFile = "./shenzhen2013-shifted-formated.osm";
+		String mapFile = "./hangzhou.osm";
+		
 		String edgesOut = "./data/bus_point_candidate/edges.txt";
 		Map map = new Map(mapFile);
 		Output.outputEdgesTxt(edgesOut, map);
 		Tool.log("Done: map generation.\n---------------------------------");
-		
+
 		String[] routes = {
-				/*test routes*/
-				"B6364_2", 
-				"B6374_1", "B6374_2",
-//				"B6384_1", "B6384_2",
-//				"M2163_1", "M2173_1"
-				};
+				/* test routes */
+				//"B6364_2", "B6374_1", "B6374_2",
+				// "B6384_1", "B6384_2",
+				// "M2163_1", "M2173_1"
+		};
 		extractSubroutes(routes); // call bash to extract subroutes
-		
-		for(String route: routes) {	
+
+		for (String route : routes) {
 			// path of passes
 			String path = "./data/bus_gps/R" + route + "/";
 			// read # of passes
-			BufferedReader br = new BufferedReader(new FileReader(path+"R"+route+".count"));
+			BufferedReader br = new BufferedReader(new FileReader(path + "R" + route + ".count"));
 			int n = Integer.parseInt(br.readLine());
 			br.close();
-			
-			//****************** st-matching **********************
+
+			// ****************** st-matching **********************
 			// get point-candidate pairs in each pass
-			for(int i = 1; i <= n; i += 1) {
+			for (int i = 1; i <= n; i += 1) {
 				Tool.log(String.format("Start: route %s_%s st-matching.", route, i));
 				CandidatePreparation cp = getCandidatePreparation(map, route, 0, 0, i);
 				st_matching(map, route, 0, cp, 0, i);
-				//String candfile = String.format("./data/bus_point_candidate/bus_candidateMatched_R%s_%s_%s_%s.OSM", route, i, pointGap, bias);
+				// String candfile =
+				// String.format("./data/bus_point_candidate/bus_candidateMatched_R%s_%s_%s_%s.OSM",
+				// route, i, pointGap, bias);
 				Tool.log(String.format("Done: %s_%s st-matching!\n---------------------------------", route, i));
 			}
 		}
 		Tool.log("All done!");
 	}
-	
-	public static void st_matching(Map map, String route, int pointGap, CandidatePreparation cp, int bias, int i) throws IOException {	
-		//**************** spatial and temporal analysis ************
+
+	public static void st_matching(Map map, String route, int pointGap, CandidatePreparation cp, int bias, int i)
+			throws IOException {
+		// **************** spatial and temporal analysis ************
 		double mu = 0;
 		double sigma = 20;
 		STAnalysis sta = new STAnalysis(map, cp, mu, sigma, true, true, true);
 		Tool.log("Done: spatial-temporal analysis.");
-		//**************** result matching **************************
-		String candidatesMatchedOsm = String.format("./data/bus_point_candidate/bus_candidateMatched_R%s_%s_%s_%s.OSM", route, i, pointGap, bias);
+		// **************** result matching **************************
+		String candidatesMatchedOsm = String.format("./data/bus_point_candidate/bus_candidateMatched_R%s_%s_%s_%s.OSM",
+				route, i, pointGap, bias);
 		ResultMatching rm = new ResultMatching(sta, cp);
-		Output.outputCandidatesMatchedOsm(candidatesMatchedOsm, rm, cp.getTimes());		
+		Output.outputCandidatesMatchedOsm(candidatesMatchedOsm, rm, cp.getTimes());
 		Tool.log("Done: result matching.");
 	}
-	
-	public static CandidatePreparation getCandidatePreparation(Map map, String route, int pointGap, int bias, int i) throws IOException {
+
+	public static CandidatePreparation getCandidatePreparation(Map map, String route, int pointGap, int bias, int i)
+			throws IOException {
 		String gpsFile = String.format("./data/bus_gps/R%s/R%s_%s.txt", route, route, i);
-		String candidatesOutOsm = String.format("./data/bus_point_candidate/bus_candidate_R%s_%s_%s_%s.OSM", route, i, pointGap, bias);
-		String pointsOutOsm = String.format("./data/bus_point_candidate/bus_point_R%s_%s_%s_%s.OSM", route, i, pointGap, bias);
+		String candidatesOutOsm = String.format("./data/bus_point_candidate/bus_candidate_R%s_%s_%s_%s.OSM", route, i,
+				pointGap, bias);
+		String pointsOutOsm = String.format("./data/bus_point_candidate/bus_point_R%s_%s_%s_%s.OSM", route, i, pointGap,
+				bias);
 		int searchRadius = 100;
 		int maxCandidateNo = 5;
 		CandidatePreparation cp = new CandidatePreparation(gpsFile, map, searchRadius, maxCandidateNo, pointGap, bias);
@@ -78,23 +87,26 @@ public class MapMatching{
 		Tool.log(String.format("Done: candidation preparation.", route, i));
 		return cp;
 	}
-	
+
 	public static int getIndexOf(ArrayList<Candidate> cands, Candidate cand) {
-		if(cand == null) return -1;
-		for(int i = 0; i < cands.size(); i++) {
-			if(cands.get(i).lon==cand.lon && cands.get(i).lat==cand.lat) {
+		if (cand == null)
+			return -1;
+		for (int i = 0; i < cands.size(); i++) {
+			if (cands.get(i).lon == cand.lon && cands.get(i).lat == cand.lat) {
 				return i;
 			}
 		}
 		return -1;
 	}
-	
+
 	public static void extractSubroutes(String[] routes) throws InterruptedException, IOException {
 		HashSet<String> routeSet = new HashSet<String>();
-		for(String r: routes) routeSet.add(r.split("_")[0]);
+		for (String r : routes)
+			routeSet.add(r.split("_")[0]);
 		StringBuilder sb = new StringBuilder();
-		for(String r: routeSet) sb.append(r + " ");
-		String[] cmd = {"sh", "-c", "cd ./data/bus_gps; bash run_extractor.sh \"" + sb.toString() + "\""};
+		for (String r : routeSet)
+			sb.append(r + " ");
+		String[] cmd = { "sh", "-c", "cd ./data/bus_gps; bash run_extractor.sh \"" + sb.toString() + "\"" };
 		Tool.callShell(cmd);
 	}
 }
--- src/map/Map.java
+++ src/map/Map.java
@@ -12,30 +12,31 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Map {
-	
-	private HashMap<NodeId, Node> nodes; //nodeId(key) node
-	public HashSet<Edge> edgeSet;		
+
+	private HashMap<NodeId, Node> nodes; // nodeId(key) node
+	public HashSet<Edge> edgeSet;
 	public HashMap<Node, ArrayList<Edge>> outEdges;
 	public HashMap<Node, ArrayList<Edge>> inEdges;
-	
+
 	public Map() {
-		
+
 	}
-	
-	public Map(String mapFile) throws IOException{
-		this.setNodes(mapFile); //setNodeSet(mapFile);
-		this.setEdges(mapFile); //setEdgeSet(mapFile);
+
+	public Map(String mapFile) throws IOException {
+		this.setNodes(mapFile); // setNodeSet(mapFile);
+		this.setEdges(mapFile); // setEdgeSet(mapFile);
 		this.setOutAndInEdges();
 	}
-	
-	private void setNodes(String mapFile) throws IOException{
+
+	private void setNodes(String mapFile) throws IOException {
 		this.nodes = new HashMap<NodeId, Node>();
 		BufferedReader br = new BufferedReader(new FileReader(mapFile));
 		String sBuf = null;
 		String regex = "node id=\"(.*?)\" lat=\"(.*?)\" lon=\"(.*?)\"";
-		while((sBuf = br.readLine()) != null){
+		while ((sBuf = br.readLine()) != null) {
 			Matcher m = Pattern.compile(regex).matcher(sBuf);
-			if(!m.find()) continue;
+			if (!m.find())
+				continue;
 			String nodeId = m.group(1);
 			double lon = Double.parseDouble(m.group(3));
 			double lat = Double.parseDouble(m.group(2));
@@ -44,83 +45,88 @@ public class Map {
 		}
 		br.close();
 	}
-	
-	private void setEdges(String mapFile) throws IOException{
+
+	private void setEdges(String mapFile) throws IOException {
 		this.edgeSet = new HashSet<Edge>();
 		String way = "";
 		boolean wayFlag = false;
 		BufferedReader br = new BufferedReader(new FileReader(mapFile));
 		String sBuf = null;
-		while((sBuf = br.readLine()) != null){
-			if(sBuf.contains("<way")){
+		while ((sBuf = br.readLine()) != null) {
+			if (sBuf.contains("<way")) {
 				wayFlag = true;
 				way = "";
-			}
-			else if(sBuf.contains("</way>")){
+			} else if (sBuf.contains("</way>")) {
 				way += sBuf;
 				wayFlag = false;
-//				if(!way.contains("highway")) continue;
+				// if(!way.contains("highway")) continue;
 				String wayId = null;
 				String wayType = null;
 				boolean oneWay = false;
 				Matcher mWayId = Pattern.compile("<way id=\"(.*?)\"").matcher(way);
 				Matcher mWayType = Pattern.compile("<tag k=\"highway\" v=\"(.*?)\"/>").matcher(way);
 				Matcher mOneWay = Pattern.compile("<tag k=\"oneway\" v=\"(.*?)\"/>").matcher(way);
-				if(mWayId.find()) wayId = mWayId.group(1);
-				if(mWayType.find()) wayType = mWayType.group(1);
-			    if(mOneWay.find() && mOneWay.group(1).equals("yes")) oneWay = true;
-//			    if(wayType == null) continue; //not bus-used wayType
-//			    if(Speed.get(wayType) == -1) continue;
-			    Matcher mNodeId = Pattern.compile("<nd ref=\"(.*?)\"(.*?)/>").matcher(way);
-			    int eIndex = 0;
-		    	Node lastNode = null;
-		    	Node nowNode = null;
-		    	NodeId nowId = null;
-		    	while(mNodeId.find()){
-		    		nowId = new Node().new NodeId(mNodeId.group(1));
-		    		nowNode = this.nodes.get(nowId);
-		    		if(lastNode == null){
-		    			lastNode = nowNode;
-		    			continue;
-		    		}
-		    		if(oneWay == true){
-		    			String edgeId = wayId + "_" + (++eIndex);
-			    		Edge edge = new Edge(edgeId, lastNode, nowNode, wayType, oneWay);
-			    		this.edgeSet.add(edge);
-				    }else{
-				    	String edgeId1 = wayId + "_" + (++eIndex);
-				    	String edgeId2 = wayId + "_" + (++eIndex);
-			    		Edge edge1 = new Edge(edgeId1, lastNode, nowNode, wayType, oneWay);
-			    		Edge edge2 = new Edge(edgeId2, nowNode, lastNode, wayType, oneWay);
-			    		this.edgeSet.add(edge1);
-			    		this.edgeSet.add(edge2);
-				    }
-		    		lastNode = nowNode;
-		    	}			    
+				if (mWayId.find())
+					wayId = mWayId.group(1);
+				if (mWayType.find())
+					wayType = mWayType.group(1);
+				if (mOneWay.find() && mOneWay.group(1).equals("yes"))
+					oneWay = true;
+				// if(wayType == null) continue; //not bus-used wayType
+				// if(Speed.get(wayType) == -1) continue;
+				Matcher mNodeId = Pattern.compile("<nd ref=\"(.*?)\"(.*?)/>").matcher(way);
+				int eIndex = 0;
+				Node lastNode = null;
+				Node nowNode = null;
+				NodeId nowId = null;
+				while (mNodeId.find()) {
+					nowId = new Node().new NodeId(mNodeId.group(1));
+					nowNode = this.nodes.get(nowId);
+					if (lastNode == null) {
+						lastNode = nowNode;
+						continue;
+					}
+					if (oneWay == true) {
+						String edgeId = wayId + "_" + (++eIndex);
+						Edge edge = new Edge(edgeId, lastNode, nowNode, wayType, oneWay);
+						this.edgeSet.add(edge);
+					} else {
+						String edgeId1 = wayId + "_" + (++eIndex);
+						String edgeId2 = wayId + "_" + (++eIndex);
+						Edge edge1 = new Edge(edgeId1, lastNode, nowNode, wayType, oneWay);
+						Edge edge2 = new Edge(edgeId2, nowNode, lastNode, wayType, oneWay);
+						this.edgeSet.add(edge1);
+						this.edgeSet.add(edge2);
+					}
+					lastNode = nowNode;
+				}
 			}
-			if(wayFlag == true)	way += sBuf + "\n";
+			if (wayFlag == true)
+				way += sBuf + "\n";
 		}
 		br.close();
 	}
-	
-	private void setOutAndInEdges(){
+
+	private void setOutAndInEdges() {
 		this.outEdges = new HashMap<Node, ArrayList<Edge>>();
 		this.inEdges = new HashMap<Node, ArrayList<Edge>>();
-		for(Edge edge: this.edgeSet){
+		for (Edge edge : this.edgeSet) {
 			Node nodeStart = edge.nodeStart;
 			Node nodeEnd = edge.nodeEnd;
-			if(this.outEdges.containsKey(nodeStart)) this.outEdges.get(nodeStart).add(edge);
-			else{
+			if (this.outEdges.containsKey(nodeStart))
+				this.outEdges.get(nodeStart).add(edge);
+			else {
 				ArrayList<Edge> edgeList = new ArrayList<Edge>();
 				edgeList.add(edge);
 				this.outEdges.put(nodeStart, edgeList);
 			}
-			if(this.inEdges.containsKey(nodeEnd)) this.inEdges.get(nodeEnd).add(edge);
-			else{
+			if (this.inEdges.containsKey(nodeEnd))
+				this.inEdges.get(nodeEnd).add(edge);
+			else {
 				ArrayList<Edge> edgeList = new ArrayList<Edge>();
 				edgeList.add(edge);
 				this.inEdges.put(nodeEnd, edgeList);
 			}
-		}		
+		}
 	}
 }
